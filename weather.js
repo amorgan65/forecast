887f1fbb4cfbe78d82e4aab4eaafb3c67c877c70
Alec Morgan <alec.morgan@berkeley.edu>
2022-07-12 16:21:06 -0700

    Initial commit

---
+++ weather.js
@@ -0,0 +1,9 @@
+//check for location settings, get lat & lon
+//if not, ask for location input
+//api request by lat&lon for weather info
+//parse json info for temp, maybe more?
+
+//maybe add way to save list of locations? to easily access again
+//account creation? or what?
+
+
