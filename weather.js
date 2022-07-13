c7781e2cfd1397ff2ea5e729a9029a8432d26145
Alec Morgan <alec.morgan@berkeley.edu>
2022-07-13 14:18:13 -0700

    Setting up project file system

--- weather.js
+++ weather.js
@@ -1,11 +1,14 @@
-//check for location settings, get lat & lon
-//if not, ask for location input
+//check for location settings, get lat & lon if available
+//if not, prompt for box input for city and dropdown for state? 
+
 //api request by lat&lon for weather info
-//parse json info for temp, maybe more?
+//parse json info for temp, maybe more? prob more
+//display in cute way? not sure how yet
 
 //maybe add way to save list of locations? to easily access again
-//account creation? or what?
+//with list of locations. should be able to add to list or remove any from list. array?
 
+//account creation? or what? could array of saved locations. Array of lat,lon pairs? But, save as cookie maybe?
+//making accounts requires database and hashing?
 
-
-
+//refresh automatically? or by button maybe to just get new call to api for new data
